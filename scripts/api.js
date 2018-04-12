fcaaddaa40a6ddd633321cdfaf6f0e19fefac95a
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-12 14:59:15 -0500

    Build API url so we can change username easily

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,9 @@
 const api = (()=>{
+  const BASE_URL = "https://thinkful-list-api.herokuapp.com";  
+  const USERNAME = "jeffrey";
+  const API_URL = `${BASE_URL}/${USERNAME}`;
+
   return {
   };
+
 })();
