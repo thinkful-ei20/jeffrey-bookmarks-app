e409a9624e47a31cecbded728b624b8f673ca041
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-12 16:46:21 -0500

    Add findById function

--- scripts/store.js
+++ scripts/store.js
@@ -7,12 +7,17 @@ const store = (() => {
     this.items.push(item);
   };
 
+  const findById = function(id) {
+    return this.items.find(item => item.id === id);
+  };
+
   return {
     items: [],
     error: null,
 
     setError,
     addItem,
+    findById,
   };
 
 })();
