cb7e35e8c57b5694e94ea569f1d4440d7b09a8ee
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-12 15:00:31 -0500

    Add getItems function for GET /bookmarks endpoint

--- scripts/api.js
+++ scripts/api.js
@@ -2,8 +2,13 @@ const api = (()=>{
   const BASE_URL = "https://thinkful-list-api.herokuapp.com";  
   const USERNAME = "jeffrey";
   const API_URL = `${BASE_URL}/${USERNAME}`;
+  
+  const getItems = (callback) => {
+    $.getJSON(API_URL + "/bookmarks", callback);
+  };
 
   return {
+    getItems,
   };
 
 })();
