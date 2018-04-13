a68627787ea27bedeb99ca5270bc73ea5a530762
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-12 22:27:23 -0500

    Create item with api

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -28,6 +28,17 @@ const bookmarkList = (() => {
   const handleNewItemSubmit = () => {
     $('#js-bookmark-list-form').submit((event) => {
       event.preventDefault();
+      const newItemTitle = $('.js-bookmark-list-entry-title').val();
+      const newItemUrl = $('.js-bookmark-list-entry-url').val();
+      api.createItem({title: newItemTitle, url: newItemUrl},
+        (newItem) => {
+          store.addItem(newItem);
+          render();
+        },
+        (err) => {
+          store.setError(err);
+          render();
+        });
     });
   };
 
