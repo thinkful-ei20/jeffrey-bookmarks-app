4614e451274ae96f3bcc4fc7b6f01665fc9fe2a4
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-13 20:56:17 -0500

    Refactor api

--- scripts/api.js
+++ scripts/api.js
@@ -1,50 +1,77 @@
-const api = (()=>{
-  const BASE_URL = "https://thinkful-list-api.herokuapp.com";  
-  const USERNAME = "jeffrey";
-  const API_URL = `${BASE_URL}/${USERNAME}`;
-  
+'use strict';
+/* global jQuery */
+
+// eslint-disable-next-line no-unused-vars
+const api = (($) => { 
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/';
+  const USERNAME = 'jeffrey';
+  const ENDPOINT = '/bookmarks';
+  const API_URL = `${BASE_URL}${USERNAME}${ENDPOINT}`;
+  const CONTENT_TYPE = 'application/json';
+  const DATA_TYPE = 'json';
+
+  let isInitialized = false;
+
   const getItems = (callback) => {
-    $.getJSON(API_URL + "/bookmarks", callback);
+    console.log('`api.getItems` ran'); // eslint-disable-line no-console
+    doWork({
+      url: API_URL,
+      success: callback,
+    });
   };
 
   const createItem = (item, onSuccess, onError) => {
-    const newItem = JSON.stringify(item);
-    $.ajax({
-      url: API_URL + "/bookmarks",
-      method: "POST",
-      contentType: "application/json",
-      data: newItem,
+    console.log('`api.createItem` ran'); // eslint-disable-line no-console
+    doWork({
+      url: API_URL,
+      method: 'POST',
+      data: JSON.stringify(item),
       success: onSuccess,
       error: onError,
     });
   };
 
   const updateItem = (id, updateData, onSuccess, onError) => {
-    const updateItem = JSON.stringify(updateData);
-    $.ajax({
-      url: API_URL + "/bookmarks/" + id,
-      method: "PATCH",
-      contentType: "application/json",
-      data: updateItem,
+    console.log('`api.updateItem` ran'); // eslint-disable-line no-console
+    doWork({
+      url: `${API_URL}/${id}`,
+      method: 'PATCH',
+      data: JSON.stringify(updateData),
       success: onSuccess,
       error: onError,
     });
   };
 
   const deleteItem = (id, onSuccess, onError) => {
-    $.ajax({
-      url: API_URL + "/bookmarks/" + id,
-      method: "DELETE",
+    doWork({
+      url: `${API_URL}/${id}`,
+      method: 'DELETE',
       success: onSuccess,
       error: onError,
     });
   };
 
+  const doWork = (work) => {
+    console.log('`api.doWork` ran'); // eslint-disable-line no-console
+    if(!isInitialized) {
+      init();
+    }
+    
+    $.ajax(work);
+  };
+  
+  const init = () => {
+    console.log('`api.init` ran'); // eslint-disable-line no-console
+    $.ajaxSetup({
+      contentType: CONTENT_TYPE,
+      dataType: DATA_TYPE,
+    });
+  };
+  
   return {
     getItems,
     createItem,
     updateItem,
     deleteItem,
   };
-
-})();
+})(jQuery);
